5588c88f1d073f709eb4f684a7a9f23f42d75460
stephenfranz22@gmail.com <stephenfranz22@gmail.com>
2020-12-22 15:11:54 -0500

    fix env 3

--- personal_portfolio_v2/settings.py
+++ personal_portfolio_v2/settings.py
@@ -11,9 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import os
 
-from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = 'vvbgot#c^c6!1lcmasdfadf-4aw5ekiow2ycp(6t0imx%t(&rn-31%ge9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
