0e0bbd4eb3c4bfbb6178cd6343c3412527f1a726
Steve <stephenfranz22@gmail.com>
2020-12-22 18:39:19 +0000

    add git ignore

--- personal_portfolio_v2/settings.py
+++ personal_portfolio_v2/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'vvbgot#c^c6!1lcm-4aw5ekiow2ycp(6t0imx%t(&rn-31%ge9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['franz242.pythonanywhere.com']
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# MEDIA_URL = '/media/'
+STATIC_ROOT = BASE_DIR / 'static'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 # STATIC_ROOT = BASE_DIR / 'media'
